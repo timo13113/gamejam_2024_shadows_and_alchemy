e414b0b6ba3d6901c3a74e5ee643bda625e9aed0
timo13113 <timger2357@gmail.com>
2024-07-21 21:11:00 +0300

    +particles shadows and movement via mouse

--- player_movement.gd
+++ player_movement.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 15000
+var toggle = false
+
+func _input(event):
+	if event.is_action_pressed("toggle_move"):
+		toggle = true
+	if event.is_action_released("toggle_move"):
+		toggle = false
 
 func _physics_process(delta):
-	
-	velocity = speed * delta * Input.get_vector("ingame_move_left", "imgame_move_right", "ingame_move_up", "ingame_move_down")
+	look_at(get_global_mouse_position())
+	#velocity = speed * delta * transform.x * int(toggle)
+	velocity = speed * delta * transform.x
+	#velocity = speed * delta * Input.get_vector(
+		#"ingame_move_left", "ingame_move_right", 
+		#"ingame_move_up", "ingame_move_down")
 	move_and_slide()
 	
